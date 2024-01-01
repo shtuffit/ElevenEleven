a98b95a8d5130e35c27a446da2d4841faa620d7d
Chris Montanaro <monty@shtuff.it>
2024-01-01 14:52:11 -0800

    E02 update

--- E02/clock.sh
+++ E02/clock.sh
@@ -3,12 +3,12 @@
 countdown()
 (
   secs=$1
-  while [ $secs -gt 0 ]
+  while [ $secs -ge 0 ]
   do
-    sleep 1 &
-    printf "\r%02d:%02d" $(( (secs/60)%60)) $((secs%60))
+    read -t 0.97 # sleep workaroud to allow for execution time
+    printf "\r%02d:%02d" $(( (secs/60)%60)) $((secs%60)) &
     secs=$(( $secs - 1 ))
-    wait
+    wait 
   done
   echo
 )
